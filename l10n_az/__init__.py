357ba286171fecd469da9ae746ad319a040d7c80
clxoid <clxoid>
2022-01-06 19:48:22 +0000

    final edit completed

--- l10n_az/__init__.py
+++ l10n_az/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) 2021 ERPGO LLC (<http://erpgo.az>).
+# Copyright (C) 2022 ERPGO LLC (<http://erpgo.az>).
 from odoo import api, SUPERUSER_ID
 
 def post_init_hook(cr, registry):
